c2302445405b31bb043f1df546217bd549c64ca1
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-19 02:43:59 +0600

    simple react firebase authentication

---
+++ _steps.js
@@ -0,0 +1,15 @@
+/*
+1. visit: console.firebase.google.com
+2. create a new firebase project 
+3. visit doc (go to docs): Build > Authentication > Web > Getting Started 
+4. Register web app > firebase project home > click web > give the name and Register 
+5. Install Firebase for our project: npm install firebase 
+6. Danger: Do not put firebase configuration in your project 
+7. export firebase app from firebase.init.js
+8. create auth using getAuth from firebase by using app from firebase.init.js
+9. create a google auth provider 
+10. go to firebase > build > authentication > sign in method 
+11. enable google sign in method with gmail
+12. Create a button for google sign in method with a click handler 
+13.  
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBK317OwTMpySo4ndtxaXExkNcBGaUNBlw",
+  authDomain: "simple-firebase-authenti-af5fc.firebaseapp.com",
+  projectId: "simple-firebase-authenti-af5fc",
+  storageBucket: "simple-firebase-authenti-af5fc.appspot.com",
+  messagingSenderId: "171608652700",
+  appId: "1:171608652700:web:5a0eaf63126f546d406659"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
